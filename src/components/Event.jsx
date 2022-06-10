29a4109140a566a1f23d968dfc0d6569ae0f32f0
kongman551 <eggy551@gmail.com>
2022-06-10 12:31:45 -0400

    Initial commit

---
+++ src/components/Event.jsx
@@ -0,0 +1,15 @@
+const Event = () => {
+    return <div className="event-container">
+        <div className="event-image">
+            <img src="https://via.placeholder.com/150" alt="event" />
+        </div>
+        <div className="event-info">
+            <h1>Event Title</h1>
+            <p>Event Date</p>
+            <p>Event Time</p>
+            <p>Event Description</p>
+        </div>
+    </div>;
+};
+
+export default Event;
---
+++ src/components/EventsList.jsx
@@ -0,0 +1,15 @@
+import Event from './Event.jsx'
+
+const EventsList = () => {
+    return (
+        <div className='events-container'>
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+            <Event />
+        </div>
+    )
+};
+
+export default EventsList;
