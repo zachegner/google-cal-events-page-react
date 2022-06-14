6de463d23b34df3d88a2355661d32954df35d685
kongman551 <eggy551@gmail.com>
2022-06-14 15:31:26 -0400

    adds images to event details

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,15 +1,20 @@
 const Event = ({ key, event }) => {
-    console.log(event)
+
+    const { name, description, start, end, location, image, dateRange, link } = event;
 
     return <div className="event-container">
-        <div className="event-image">
-            <img src='#' alt="Event Image" />
+        <div className="event-image-container">
+            {!image ? <img className="placeholder-image" src="https://via.placeholder.com/150x150" alt="event-image" /> : <img className="event-image" src={`https://drive.google.com/uc?export=view&id=${image[0].fileId}`} alt={`${name} Image`} />}
+
         </div>
         <div className="event-info">
-            <h1>{event.name}</h1>
-            <p>{event.start.day}, {event.start.month} {event.start.date}</p>
-            <p>{event.start.time}</p>
-            <p>{event.description}</p>
+            <h1>{name}</h1>
+            <p>{dateRange}</p>
+            <p>{start.month} {start.date}</p>
+            <p>{start.time}</p>
+            <p>{description}</p>
+            <a href={link}>Link</a>
+            
         </div>
     </div>;
 };
