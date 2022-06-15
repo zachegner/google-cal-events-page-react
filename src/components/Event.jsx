c7c91e6c81ab775dd9b0f4aa8cc29d4d937895f5
kongman551 <eggy551@gmail.com>
2022-06-15 12:47:40 -0400

    edits formatting and mobile response

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,21 +1,42 @@
+import { Button } from "@mui/material";
+import Divider from "@mui/material/Divider";
+
 const Event = ({ key, event }) => {
 
     const { name, description, start, end, location, image, dateRange, link } = event;
 
-    return <div className="event-container">
-        <div className="event-image-container">
-            {!image ? <img className="placeholder-image" src="https://via.placeholder.com/150x150" alt="event-image" /> : <img className="event-image" src={`https://drive.google.com/uc?export=view&id=${image[0].fileId}`} alt={`${name} Image`} />}
+    var regex = /<html-blob>(.*?)<\/html-blob>/g
 
+    if (regex.test(description)) {
+        var result = description.match(/<html-blob>(.*?)<\/html-blob>/g).map(function(val){
+        return val.replace(/<\/?html-blob>/g,'');
+    });
+    }
+
+    
+
+    console.log(result)
+
+    /* var webLink = description.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/); */
+
+    return (
+        <div className="event-container">
+            <div className="event-image-container">
+                {!image ? <img className="placeholder-image" src="https://via.placeholder.com/150x150" alt="event-image" /> : <img className="event-image" src={`https://drive.google.com/uc?export=view&id=${image[0].fileId}`} alt={`${name} Image`} />}
+
+            </div>
+            <div className="event-info">
+                <h1>{name.toUpperCase()}</h1>
+                <h2>{start.day}, {start.month} {start.date}</h2>
+                <h4>{start.time}</h4>
+                <p>{description}</p>
+                {/* <p>{webLink}</p> */}
+                <button className="info-btn">EVENT LINK</button> 
+            </div>
+            <Divider></Divider>
+            <br />
         </div>
-        <div className="event-info">
-            <h1>{name}</h1>
-            <h2>{start.day}, {start.month} {start.date}</h2>
-            <h4>{start.time}</h4>
-            <p>{description}</p>
-            <a href={link}>Link</a>
-            
-        </div>
-    </div>;
+    );
 };
 
 export default Event;
