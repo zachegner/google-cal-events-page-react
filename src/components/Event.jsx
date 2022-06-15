133ee23f23e257a6ae9f880446fea75d7971b403
kongman551 <eggy551@gmail.com>
2022-06-15 14:17:27 -0400

    adds link capability

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,37 +1,37 @@
-import { Button } from "@mui/material";
 import Divider from "@mui/material/Divider";
+import { useState, useEffect } from "react";
 
-const Event = ({ key, event }) => {
+
+const Event = ({ event }) => {
 
     const { name, description, start, end, location, image, dateRange, link } = event;
 
-    var regex = /<html-blob>(.*?)<\/html-blob>/g
+    const [eventUrl, setEventUrl] = useState('');
 
-    if (regex.test(description)) {
-        var result = description.match(/<html-blob>(.*?)<\/html-blob>/g).map(function(val){
-        return val.replace(/<\/?html-blob>/g,'');
-    });
-    }
+    useEffect(() => {
+        const re = /(https?:\/\/[^ ]*)/
+        if (re.test(description)) {
+            const url = re.exec(description)
+            setEventUrl(url[0])
+        }
+    }, [])
 
-    
-
-    console.log(result)
-
-    /* var webLink = description.match(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/); */
+    const newDesc = description ? description.substring(eventUrl.length) : '';
 
     return (
         <div className="event-container">
             <div className="event-image-container">
-                {!image ? <img className="placeholder-image" src="https://via.placeholder.com/150x150" alt="event-image" /> : <img className="event-image" src={`https://drive.google.com/uc?export=view&id=${image[0].fileId}`} alt={`${name} Image`} />}
+                {!image ? <img className='placeholder-image' src="src/images/lostShoeOutline.png" alt="event-image" /> : <img className="event-image" src={`https://drive.google.com/uc?export=view&id=${image[0].fileId}`} alt={`${name} Image`} />}
 
             </div>
             <div className="event-info">
-                <h1>{name.toUpperCase()}</h1>
+                <h1>{name ? name.toUpperCase() : "EVENT"}</h1>
                 <h2>{start.day}, {start.month} {start.date}</h2>
                 <h4>{start.time}</h4>
-                <p>{description}</p>
-                {/* <p>{webLink}</p> */}
-                <button className="info-btn">EVENT LINK</button> 
+                <p>{newDesc}</p>
+                {eventUrl && <form action={eventUrl}>
+                    <input className="info-btn" type="submit" value="EVENT LINK" />
+                </form>}
             </div>
             <Divider></Divider>
             <br />
